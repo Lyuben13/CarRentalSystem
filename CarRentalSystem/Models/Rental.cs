aec4adbe75c37fda1baae1191abcdff1e2a85779
LyubenAndreev <liuben.Andreev@gmail.com>
2025-07-05 11:37:53 +0300

    New file Rental.cs

---
+++ CarRentalSystem/Models/Rental.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalSystem.Models
+{
+    public class Rental
+    {
+        public int CarId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime ExpectedReturn { get; set; }
+        public DateTime? ActualReturn { get; set; }
+    }
+
+}
